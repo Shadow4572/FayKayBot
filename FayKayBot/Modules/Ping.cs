d3310372d4b5b5abeb5964694dca87d5081697d7
Marcel Riedmann <marcel.riedmann@hak-kitz.ac.at>
2018-09-25 09:10:08 +0200

    added ping command

---
+++ FayKayBot/Modules/Ping.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FayKayBot.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("pong :heartbeat: " + Context.Client.Latency + " ms");
+        }
+    }
+}
