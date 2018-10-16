3b5f58b06aca4c33702270fc48fb44732066e81a
Marcel Riedmann <marcel.riedmann@hak-kitz.ac.at>
2018-10-16 09:01:30 +0200

    Implemented reverse function

---
+++ FayKayBot/Modules/Reverse.cs
@@ -0,0 +1,28 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FayKayBot.Modules
+{
+    public class Reverse : ModuleBase<SocketCommandContext>
+    {
+        [Command("reverse")]
+        public async Task ReverseAsync([Remainder] string message)
+        {
+            var reversedMessage = message.Reverse();
+            string reversedMessageString = "";
+
+            foreach (var m in reversedMessage)
+            {
+                reversedMessageString = reversedMessageString + m.ToString();
+            }
+
+            await Context.Message.DeleteAsync();
+            await ReplyAsync($"{reversedMessageString}\n*Reversed by {Context.Message.Author.Mention}*");
+        }
+    }
+}
