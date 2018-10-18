fd9408aac14a87a23b39e8b70b96f0bf4395180f
Shadow4572 <marcel.riedmann@hak-kitz.ac.at>
2018-10-18 20:31:27 +0200

    Implemented pick command

---
+++ FayKayBot/Modules/Pick.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FayKayBot.Modules
+{
+    public class Pick : ModuleBase<SocketCommandContext>
+    {
+        [Command("pick")]
+        public async Task PickAsync([Remainder] string options)
+        {
+            #region Variables
+            string[] arOptions = options.Split('|');
+            Random rnd = new Random();
+            int rng = rnd.Next(0, arOptions.Length);
+            #endregion
+
+            if (!options.Contains("|"))
+            {
+                await ReplyAsync("Please use '|' as a separator!");
+                return;
+            }
+
+            await ReplyAsync($"I think the better option would be: **{arOptions[rng]}**");
+        }
+    }
+}
--- FayKayBot/Modules/Reverse.cs
+++ FayKayBot/Modules/Reverse.cs
@@ -13,13 +13,17 @@ namespace FayKayBot.Modules
         [Command("reverse")]
         public async Task ReverseAsync([Remainder] string message)
         {
+            #region Variables
             var reversedMessage = message.Reverse();
             string reversedMessageString = "";
+            #endregion
 
+            #region Initializing variables
             foreach (var m in reversedMessage)
             {
                 reversedMessageString = reversedMessageString + m.ToString();
             }
+            #endregion
 
             await Context.Message.DeleteAsync();
             await ReplyAsync($"{reversedMessageString}\n*Reversed by {Context.Message.Author.Mention}*");
